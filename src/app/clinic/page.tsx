4c7268fd2d25bd0d9814c2035eaf0c6aef37ac77
ergashevv <ergashevpulot@gmail.com>
2025-05-13 05:42:55 +0500

    Страница О клинике / скрыть блоки

--- src/app/clinic/page.tsx
+++ src/app/clinic/page.tsx
@@ -313,17 +313,17 @@ export default function Clinic() {
       <MedicalEquipmentSlider />
       
       {/* Передаем данные администраторов в компонент */}
-      <AdministrationSlider administrators={administrators} />
+      {/* <AdministrationSlider administrators={administrators} /> */}
       
       <CareerForm />
       
       {/* Используем форматированные сертификаты */}
-      <CertificatesSlider 
+      {/* <CertificatesSlider 
         certificates={formattedCertificates}
         title="Доверие и качество"
         description="Все наши медицинские услуги лицензированы и соответствуют строгим стандартам"
       />
-      
+       */}
       <ContactInfo />
     </main>
   );
